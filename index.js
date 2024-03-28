0927b0b801bb9123ab6dce47345fb1e180cf05a3
Tanzeela Arshad <aimaanmol37@gmail.com>
2024-03-28 21:40:05 +0500

    Email & Password Authenticator

---
+++ index.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+let yourEmail = "youexample@gmail.com";
+let yourPassword = 54321;
+const answer = await inquirer.prompt([
+    { message: "Enter your Email: ", type: "input", name: "username" },
+    { message: "Enter your Password: ", type: "number", name: "Password" },
+]);
+if (answer.username === yourEmail && answer.Password === yourPassword) {
+    console.log("You've signed in successfully");
+}
+else {
+    console.log("Username or Password doesn't match");
+}
---
+++ index.ts
@@ -0,0 +1,15 @@
+import inquirer from "inquirer";
+
+let yourEmail: string = "youexample@gmail.com";
+let yourPassword: number = 54321;
+
+const answer = await inquirer.prompt([
+  { message: "Enter your Email: ", type: "input", name: "username" },
+  { message: "Enter your Password: ", type: "number", name: "Password" },
+]);
+
+if (answer.username === yourEmail && answer.Password === yourPassword) {
+  console.log("You've signed in successfully");
+} else {
+  console.log ("Username or Password doesn't match");
+}
