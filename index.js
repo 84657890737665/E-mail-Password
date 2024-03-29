b91d44408b42b3a1339782a54ff3e9185ebc74bb
Tanzeela Arshad <aimaanmol37@gmail.com>
2024-03-29 08:56:42 +0500

    Email & Password Authenticator with more security or privacy

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import inquirer from "inquirer";
 let yourEmail = "youexample@gmail.com";
-let yourPassword = 54321;
+let yourPassword = "54321";
 const answer = await inquirer.prompt([
     { message: "Enter your Email: ", type: "input", name: "username" },
-    { message: "Enter your Password: ", type: "number", name: "Password" },
+    { message: "Enter your Password: ", type: "password", name: "Password" },
 ]);
 if (answer.username === yourEmail && answer.Password === yourPassword) {
     console.log("You've signed in successfully");
--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import inquirer from "inquirer";
 
 let yourEmail: string = "youexample@gmail.com";
-let yourPassword: number = 54321;
+let yourPassword: string = "54321";
 
 const answer = await inquirer.prompt([
   { message: "Enter your Email: ", type: "input", name: "username" },
-  { message: "Enter your Password: ", type: "number", name: "Password" },
+  { message: "Enter your Password: ", type: "password", name: "Password" },
 ]);
 
 if (answer.username === yourEmail && answer.Password === yourPassword) {
